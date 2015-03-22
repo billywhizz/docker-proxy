2e422c02f7ef6dd86b449c0cef6e6d8953f2d5a9
Andrew Johnston <apjohnsto@gmail.com>
2015-03-22 01:46:31 +0000

    remove bootstrap

--- bootstrap.sh
+++
@@ -1,7 +0,0 @@
-#cd /tmp
-#wget https://raw.githubusercontent.com/billywhizz/docker-proxy/master/alpine-proxy.tar.gz
-#docker load -i alpine-proxy.tar.gz
-#docker run -d -e PROXY_PORT=8001 -e BACKEND_PORT=80 -e PROXY_HOST=0.0.0.0 -e BACKEND_HOST=stage.oneflowcloud.com -p 8001:8001 alpine/proxy sh /app/start.sh
-#rm -f alpine-proxy.tar.gz
-docker pull $1
-docker run -d -e PROXY_PORT=8001 -e BACKEND_PORT=80 -e PROXY_HOST=0.0.0.0 -e BACKEND_HOST=stage.oneflowcloud.com -p 8001:8001 $1 sh /app/start.sh
